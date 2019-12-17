f85ca4c558871de2313ae660d286634d168ac26e
Petra Laohakul <petradish@gmail.com>
2019-12-16 22:08:33 -0500

    fix: change port to 3000

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = require('http').createServer(app)
 
