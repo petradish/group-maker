d664ad6aea267ad00a55328ca7358db23118c9b8
Petra Laohakul <petradish@gmail.com>
2019-12-16 21:47:13 -0500

    fix: server to http

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = app.listen(PORT, () => console.log(`Connected on port ${PORT}`));
+const server = require('http').createServer(app)
+
+server.listen(PORT, () => console.log(`Connected on port ${PORT}`));
+
 const socketio = require('socket.io');
 
 // handle sockets
