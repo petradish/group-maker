19862f60fef85975037c8c95b3e46eef01bcbaee
Petra Laohakul <petradish@gmail.com>
2019-12-17 19:52:21 -0500

    fix: db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./db/db');
+const db = require('./db/index');
 const morgan = require('morgan');
 const PORT = process.env.PORT || 5000;
 const app = express();
