fb95c62a79a124575bf2ce70566ef8d07d99100c
Petra Laohakul <petradish@gmail.com>
2019-12-17 19:49:16 -0500

    fix: wildcard get

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use((req, res, next) =>
 app.use('/api', require('./api'));
 // send index.html
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, 'build', 'index.html'));
  });
 
